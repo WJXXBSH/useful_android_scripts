c67fd2706df264e72a8c39660f993bb1b8738ed0
WJXXBSH <zyb_1998@outlook.com>
2019-06-27 14:37:34 +0800

    Hide cmd instead of pm and adbd

--- others/check.sh
+++ others/check.sh
@@ -4,9 +4,9 @@ if [ -f "/data/local/tmp/check_enabled" ]; then
 echo "Restoring..."
 rm -f "/data/local/tmp/check_enabled"
 mount -o remount,rw /system
-mv -f /system/bin/hw/perf_config_0.xml /system/bin/adbd
+#mv -f /system/bin/hw/perf_config_0.xml /system/bin/adbd
 mv -f /system/bin/hw/perf_config_1.xml /system/bin/dumpsys
-mv -f /system/bin/hw/perf_config_2.xml /system/bin/pm
+mv -f /system/bin/hw/perf_config_2.xml /system/bin/cmd
 pm unhide com.github.shadowsocks
 pm enable com.github.shadowsocks
 pm unhide org.telegram.messenger
@@ -28,9 +28,9 @@ pm disable com.github.shadowsocks
 pm hide org.telegram.messenger
 pm disable org.telegram.messenger
 pm disable com.android.shell
-mv -f /system/bin/adbd /system/bin/hw/perf_config_0.xml
+#mv -f /system/bin/adbd /system/bin/hw/perf_config_0.xml
 mv -f /system/bin/dumpsys /system/bin/hw/perf_config_1.xml
-mv -f /system/bin/pm /system/bin/hw/perf_config_2.xml
+mv -f /system/bin/cmd /system/bin/hw/perf_config_2.xml
 chmod 551 /data/app/
 rm -rf /data/media/0/Android/data/org.telegram.messenger/
 rm -rf /data/media/0/Android/data/com.github.shadowsocks/
