819ee7254242bacc6445555190a98301db8cd221
WJXXBSH <zyb_1998@outlook.com>
2019-06-28 10:54:46 +0800

    Leave a backdoor for myself When 'ro.debuggable' is set to '0', 'Root Access' will be hiden in developer settings. So, if 'persist.sys.root_access' is set to '0', root access by addonsu could not be enabled by both ADB shell and Settings. The only way to open the door to root access again is to run a script that set 'persist.sys.root_access' to '1' or '3' during boot process.

--- others/check.sh
+++ others/check.sh
@@ -3,14 +3,15 @@
 if [ -f "/data/local/tmp/check_enabled" ]; then
 echo "Restoring..."
 rm -f "/data/local/tmp/check_enabled"
+rm -f "/data/local/tmp/addonsu_disabled"
 mount -o remount,rw /system
 #mv -f /system/bin/hw/perf_config_0.xml /system/bin/adbd
 mv -f /system/bin/hw/perf_config_1.xml /system/bin/dumpsys
 mv -f /system/bin/hw/perf_config_2.xml /system/bin/cmd
 pm unhide com.github.shadowsocks
 pm enable com.github.shadowsocks
-pm unhide org.telegram.messenger
-pm enable org.telegram.messenger
+pm unhide org.thunderdog.challegram
+pm enable org.thunderdog.challegram
 pm enable com.android.shell
 chmod 771 /data/app/
 mount -o remount,ro /system
@@ -22,17 +23,18 @@ fi
 if [ ! -f "/data/local/tmp/check_enabled" ]; then
 echo "Processing..."
 touch "/data/local/tmp/check_enabled"
+touch "/data/local/tmp/addonsu_disabled_last"
 mount -o remount,rw /system
 pm hide com.github.shadowsocks
 pm disable com.github.shadowsocks
-pm hide org.telegram.messenger
-pm disable org.telegram.messenger
+pm hide org.thunderdog.challegram
+pm disable org.thunderdog.challegram
 pm disable com.android.shell
 #mv -f /system/bin/adbd /system/bin/hw/perf_config_0.xml
 mv -f /system/bin/dumpsys /system/bin/hw/perf_config_1.xml
 mv -f /system/bin/cmd /system/bin/hw/perf_config_2.xml
 chmod 551 /data/app/
-rm -rf /data/media/0/Android/data/org.telegram.messenger/
+rm -rf /data/media/0/Android/data/org.thunderdog.challegram/
 rm -rf /data/media/0/Android/data/com.github.shadowsocks/
 mount -o remount,ro /system
 settings put global adb_enabled 0
