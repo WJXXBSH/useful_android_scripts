1d92ae9527a88a3b39bae7f18d2cbf941e4cf087
WJXXBSH <zyb_1998@outlook.com>
2020-12-16 13:47:30 +0800

    OTA: lineage-17.1-20201216

--- others/dex2oat.sh
+++ others/dex2oat.sh
@@ -1,22 +1,15 @@
 #!/system/bin/sh
-# Move oat files to /system
+# Move Inputmethod oat files into system partition
 mount -o remount,rw /
 
-# Inputmethod
-chmod 755 /system/product/app/Inputmethod
-chmod 755 /system/product/app/Inputmethod/lib
-chmod 755 /system/product/app/Inputmethod/lib/arm64
-mkdir /system/product/app/Inputmethod/oat
-chmod 755 /system/product/app/Inputmethod/oat
-mkdir /system/product/app/Inputmethod/oat/arm64
-chmod 755 /system/product/app/Inputmethod/oat/arm64
+mkdir -p /system/product/app/Inputmethod/oat/arm64
 mv -f /data/dalvik-cache/arm64/system@product@app@Inputmethod@Inputmethod.apk@classes.dex /system/product/app/Inputmethod/oat/arm64/Inputmethod.odex
 mv -f /data/dalvik-cache/arm64/system@product@app@Inputmethod@Inputmethod.apk@classes.vdex /system/product/app/Inputmethod/oat/arm64/Inputmethod.vdex
-chmod 644 /system/product/app/Inputmethod/oat/arm64/Inputmethod.odex
-chgrp 0 /system/product/app/Inputmethod/oat/arm64/Inputmethod.odex
-chown 0 /system/product/app/Inputmethod/oat/arm64/Inputmethod.odex
-chmod 644 /system/product/app/Inputmethod/oat/arm64/Inputmethod.vdex
-chgrp 0 /system/product/app/Inputmethod/oat/arm64/Inputmethod.vdex
-chown 0 /system/product/app/Inputmethod/oat/arm64/Inputmethod.vdex
+chgrp -R root /system/product/app/Inputmethod
+chown -R root /system/product/app/Inputmethod
+chmod -R 755 /system/product/app/Inputmethod
+chmod 644 $(find /system/product/app/Inputmethod -type f)
+restorecon /system/product/app/Inputmethod/oat/arm64/Inputmethod.odex
+restorecon /system/product/app/Inputmethod/oat/arm64/Inputmethod.vdex
 
 mount -o remount,ro /
